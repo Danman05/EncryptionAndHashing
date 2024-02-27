63d7761a62e365c4abba71baa107b3b34a4d9275
Daniel Spurrell <danielspurrell@hotmail.com>
2024-02-27 14:23:50 +0100

    Secure Password

---
+++ SecurePassword/Repositories/UserRepo.cs
@@ -0,0 +1,37 @@
+﻿using SecurePassword.Interface;
+
+namespace SecurePassword.Repositories
+{
+    public class UserRepo : IUserRepo
+    {
+        public User? GetOne(int id)
+        {
+            using dataContext dataContext = new dataContext();
+            User? user = dataContext.Users.Find(id);
+            return user != null ? user : null;
+        }
+
+        public ICollection<User> GetAll()
+        {
+            using dataContext dataContext = new dataContext();
+            return dataContext.Users.ToList();
+        }
+
+        public void Create(User user)
+        {
+            using dataContext dataContext = new dataContext();
+            dataContext.Users.Add(user);
+        }
+
+        public void Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(User user)
+        {
+            using dataContext dataContext = new dataContext();
+            dataContext.Users.Remove(user);
+        }
+    }
+}
---
+++ SecurePassword/dataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurePassword
+{
+    internal class dataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=::1;Initial Catalog=SecurePassword;Integrated Security=True; Encrypt=False");
+        }
+    }
+}
