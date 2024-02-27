b03f543ddb98ec437d23b758ca5e9686563f937a
Daniel Spurrell <danielspurrell@hotmail.com>
2024-02-27 10:27:06 +0100

    project init

---
+++ SecurePassword/PBKDF2.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+
+namespace SecurePassword
+{
+    public class Pbkdf2
+    {
+        public static byte[] GenerateSalt()
+        {
+            const int saltLength = 32;
+            var randomNumberGenerator = RandomNumberGenerator.Create();
+            byte[] randomBytes = new byte[saltLength]; // Brug length parameteren til at bestemme antallet af bytes
+            randomNumberGenerator.GetBytes(randomBytes);
+
+            return randomBytes;
+        }
+
+
+        public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAlgorithmName.SHA256))
+            {
+                return rfc2898.GetBytes(32);
+            };
+
+        }
+    }
+}
